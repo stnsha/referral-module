62c6ee05a01074d4fa3e90330b45dcc677b0846d
Siti Anasuha <anasuharosli@gmail.com>
2025-07-04 16:46:56 +0800

    fix: unset in post

--- post.php
+++ post.php
@@ -90,6 +90,9 @@ $data['attachments'] = $uploadedFiles;
 
 $endpoint = 'referral';
 $response = getApiData($endpoint, $data, 'POST');
+if (is_array($response) && array_key_exists('ch', $response)) {
+    unset($response['ch']);
+}
 echo json_encode($response);
 
 exit;
